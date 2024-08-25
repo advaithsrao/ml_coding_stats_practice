4512012ba043d46375702aa4a473165d29d4cf1a
Advaith Rao <advaith652@gmail.com>
2024-08-25 15:42:20 -0400

    kmeans test

--- tests/test_ml.py
+++ tests/test_ml.py
@@ -1,5 +1,7 @@
 import pytest
+
 from ml.classification.knn import KNN
+from ml.classification.kmeans import KMeans
 from ml.helpers import load_sample_dataset, get_classification_results
 
 @pytest.fixture
@@ -13,7 +15,9 @@ def test_knn(sample_data):
     preds = knn.predict(X_test)
 
     #Check fns
-    assert 'fit'
+    assert knn.__getattribute__('fit')
+    assert knn.__getattribute__('predict')
+    assert knn.k == 1
 
     #Results 
     results = get_classification_results(y_test, preds)
@@ -21,3 +25,20 @@ def test_knn(sample_data):
     assert results['precision'] > 0.9
     assert results['recall'] > 0.9
     assert results['f1'] > 0.9
+
+def test_kmeans(sample_data):
+    X_train, X_test, _, _ = sample_data
+    kmeans = KMeans(k=3)
+    kmeans.fit(X_train)
+    preds = kmeans.predict(X_test)
+
+    #Check fns
+    assert kmeans.__getattribute__('fit')
+    assert kmeans.__getattribute__('predict')
+    assert kmeans.k == 3
+
+    #Results
+    assert len(kmeans.centroids) == 3
+    assert len(kmeans.clusters) == 3
+    assert len(preds) == len(X_test)
+    assert kmeans.calculate_inertia() > 0
